c72298ba7ecaf73716c1cdc00957257872aae38a
Tamilselvan <tamilselvan.k@arkinfotec.com>
2022-10-17 15:01:06 +0530

    # layout changes add asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,15 +5,15 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ ('assets/img/apple-icon.png') }}">
-    <link rel="icon" type="image/png" href="{{ ('assets/img/favicon.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
     <title>{{ config('app.name', 'Quizz') }}</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <link href="{{ ('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-    <link href="{{ ('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <link href="{{ ('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
-    <link id="pagestyle" href="{{ ('assets/css/argon-dashboard.css?v=2.0.4') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.css?v=2.0.4') }}" rel="stylesheet" />
     @stack('css')
 </head>
 
@@ -30,14 +30,14 @@
     </main>
     @include('admin.partials.plugins')
     <!--   Core JS Files   -->
-    <script src="{{ ('assets/js/core/popper.min.js') }}"></script>
-    <script src="{{ ('assets/js/core/bootstrap.min.js') }}"></script>
-    <script src="{{ ('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
-    <script src="{{ ('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
-    <script src="{{ ('assets/js/plugins/chartjs.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/chartjs.min.js') }}"></script>
     @include('admin.partials.themeScript')
     <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <script src="{{ ('assets/js/argon-dashboard.min.js?v=2.0.4') }}"></script>
+    <script src="{{ asset('assets/js/argon-dashboard.min.js?v=2.0.4') }}"></script>
     @stack('js')
 </body>
 
