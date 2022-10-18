feec2a6d8bee9b4bf9d954dbbb6fe5f71e3a3744
Tamilselvan <tamilselvan.k@arkinfotec.com>
2022-10-18 19:05:19 +0530

    # User list table and footer chnages

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-4">
+                <div class="card-header pb-0">
+                    <h6>Users table</h6>
+                </div>
+                <div class="card-body px-0 pt-0 pb-2">
+                    <div class="table-responsive p-0">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                                <tr>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">User
+                                    </th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Status</th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Date</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"> Actions </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($users as $user)
+                                <tr>
+                                    <td>
+                                        <div class="d-flex px-2 py-1">
+                                            <div>
+                                                <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3"
+                                                    alt="user1">
+                                            </div>
+                                            <div class="d-flex flex-column justify-content-center">
+                                                <h6 class="mb-0 text-sm"> {{ $user->name }} </h6>
+                                                <p class="text-xs text-secondary mb-0"> {{ $user->email }} </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="align-middle text-center text-sm">
+                                        <span class="badge badge-sm bg-gradient-success">Online</span>
+                                    </td>
+                                    <td class="align-middle text-center">
+                                        <span class="text-secondary text-xs font-weight-bold"> {{ date('d-m-Y', strtotime($user->created_at)) }} </span>
+                                    </td>
+                                    <td class="align-middle">
+                                        <a href="javascript:;" class="btn bg-gradient-info font-weight-bold text-xs"
+                                            data-toggle="tooltip" data-original-title="Edit user">
+                                            Edit
+                                        </a> &nbsp&nbsp
+                                        <a href="javascript:;" class="btn bg-gradient-danger font-weight-bold text-xs"
+                                        data-toggle="tooltip" data-original-title="Delete user">
+                                        Delete
+                                    </a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
