a2d2c26ae24e7c0788b8a52d58df512f2bbb0b22
kokilavani.p <kokilavani.p@arkinfotec.com>
2022-11-22 19:40:23 +0530

    Unwanted space removed

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,4 +1,3 @@
-
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
 
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
