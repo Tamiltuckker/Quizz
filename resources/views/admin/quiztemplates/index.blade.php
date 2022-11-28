53c26eddfa8e5992ec147f20a3898b08eaec8c50
Tamilselvan <tamilselvan.k@arkinfotec.com>
2022-11-28 17:00:48 +0530

    # sidebar chnages and dashboard page show counts dynamically

--- resources/views/admin/quiztemplates/index.blade.php
+++ resources/views/admin/quiztemplates/index.blade.php
@@ -36,14 +36,14 @@
                                         <td>
                                             <div class="d-flex px-2 py-1">
                                                 <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="mb-0 text-sm">{{ $quizTemplate->category->name }}</h6>
+                                                    <h6 class="mb-0 text-sm">{{ @$quizTemplate->category->name }}</h6>
                                                 </div>
                                             </div>
                                         </td>
                                         <td>
                                             <div class="d-flex px-2 py-1">
                                                 <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="mb-0 text-sm">{{$quizTemplate->quiz_questions->count()}}</h6>
+                                                    <h6 class="mb-0 text-sm">{{ @$quizTemplate->quiz_questions->count() }}</h6>
                                                 </div>
                                             </div>
                                         </td>             
