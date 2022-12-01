f4a06ecdf55bc7861299db6881b0a51b8c5c80f4
kokilavani.p <kokilavani.p@arkinfotec.com>
2022-12-01 16:08:04 +0530

    frontend issue fixes -completed

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -30,31 +30,26 @@
                         <p>PHP Quiz templates with Question. Register your account and explore the Quiz</p>
                     </div>
                 </div> 
-            @endguest
+            @endguest 
 
-            @auth
+             @auth
                 @foreach ($categories as $category)
                     <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
                         <div class="icon-box">
                             <div><img src="{{ asset('/storage/image/' . $category->image->image) }}" width="50" height="50"></div>
                             <br>
-                            <h4><a href="{{ route('user.dashboard.gettemplates',$category->slug) }}">{{ $category->name }}</a></h4>
+                            @php $userEmailVerfication = \App\Models\User::where('id', Auth::user()->id)->first();@endphp
+                             @if ($userEmailVerfication->email_verified_at !== null)
+                                <h4 class="useremail"><a href="{{ route('user.dashboard.gettemplates',$category->slug) }}">{{$category->name  }}</a></h4>
+                            @else
+                                <h4 onclick="userEmail()"><a href="#">{{ $category->name }}</a></h4>
+                            @endif
                             <p> Total Templates:{{count($category->quiz_templates)}}</p>
                             <br>
                             <p> Total Question:{{ count($category->quiz_questions()->get()) }}</p>
                             <br>
-                            <div class="icon"><i class="bx bxl-dribbble"></i></div>
-                            @php $userEmailVerfication = \App\Models\User::where('id', Auth::user()->id)->first();@endphp
-                            
-                            @if ($userEmailVerfication->email_verified_at !== null)
-                                <h4 class="useremail"><a
-                                        href="{{ route('user.dashboard.gettemplates') }}">{{ $category }}</a></h4>
-                            @else
-                                <h4 onclick="userEmail()"><a href="#">{{ $category }}</a></h4>
-                            @endif
-
-                            <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
-                        </div>
+                            <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p> 
+                        </div> 
                     </div>
                 @endforeach
                 @endauth
