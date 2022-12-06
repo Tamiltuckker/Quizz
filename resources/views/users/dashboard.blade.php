dc408d157948774b3b8564754624508f74fca7ac
Tamilselvan <tamilselvan.k@arkinfotec.com>
2022-12-06 20:42:16 +0530

    # attend Categories user count displayed frontend

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -12,7 +12,6 @@
                     <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
                         <div class="icon-box">
                             <div>
-
                             @if($category->image !== null)
                                 <img src="{{ asset('/storage/image/' . $category->image->image) }}" width="50" height="50">
                             @else
@@ -20,6 +19,10 @@
                             @endif
                             </div>
                             <br>
+                            @php 
+                                $attendUsersCount = \App\Models\QuizAnswer::where('category_id',$category->id)->get()
+                                                      ->groupBy('user_id')->count();
+                            @endphp
                             @auth
                                 @php $userEmailVerfication = \App\Models\User::where('id', Auth::user()->id)->first();@endphp
                                 @if ($userEmailVerfication->email_verified_at !== null)
@@ -30,11 +33,11 @@
                             @endauth
                             @guest
                                 <h4><a href="{{ route('login') }}">{{ $category->name }}</a></h4> 
+                                <p>Users :{{ $attendUsersCount }} </p>
                             @endguest
+                            <p> Attend Users :{{ $attendUsersCount }} </p>
                             <p> Total Templates:{{count($category->quiz_templates)}}</p>
-                            <br>
                             <p> Total Question:{{ count($category->quiz_questions()->get()) }}</p>
-                            <br>
                             <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p> 
                         </div> 
                     </div>
