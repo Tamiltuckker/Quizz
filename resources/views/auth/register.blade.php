42eb9142dd654d87653b985aa151d9e5baf724bc
Tamilselvan <tamilselvan.k@arkinfotec.com>
2022-12-12 18:44:00 +0530

    # register checked remove

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -109,8 +109,7 @@
                             </div>
 
                             <div class="form-check form-check-info text-start">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"
-                                    checked>
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
                                 <label class="form-check-label" for="flexCheckDefault">
                                     I agree the <a href="javascript:;" class="text-dark font-weight-bolder">Terms and
                                         Conditions</a>
