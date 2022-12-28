e0eaf2095786609cd9d1019f4e5a97be691a32be
umadevi <umadevi.k@arkinfotec.com>
2022-12-28 15:08:06 +0530

    removed privacy policy from footer

--- resources/views/users/partials/footer.blade.php
+++ resources/views/users/partials/footer.blade.php
@@ -8,7 +8,6 @@
                         <h3 class="logo me-auto me-lg-0"><a href="{{ route('user.dashboard.index') }}">Quiz<span>.</span></a></h3>
                     </div>
                 </div>
-
                 <div class="col-lg-2 col-md-6 footer-links">
                     <h4 class="logo me-auto me-lg-0"><a href="{{ route('user.dashboard.index') }}">Quiz<span>.</span></a></h1>
                     <ul>
@@ -17,7 +16,6 @@
                         <li><i class="bx bx-chevron-right"></i> <a href="{{ route('user.contact') }}">Contact</a></li>                        
                     </ul>
                 </div>
-
                 <div class="col-lg-3 col-md-6 footer-links">
                     <h4>Our Categories</h4>
                     <ul>                       
@@ -50,7 +48,6 @@
             </div>
         </div>
     </div>
-
     <div class="container">
         <div class="copyright">
             &copy; Copyright <strong><span>Quiz</span></strong>. All Rights Reserved
